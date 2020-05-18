ab33d85f3733e86c018c3b74298aba60b97bcd5b
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-17 22:58:57 -0400

    Commented out incomplete function and wrote some notes

---
+++ bjtrial.js
@@ -0,0 +1,105 @@
+
+let cards = {
+    deck: [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10],
+    player1Cards: [],
+    player1CardsTotal: 0,
+    dealerCards: [],
+    dealerCardsTotal: 0,
+    busted: 0
+  }
+  
+  let cardMethods = {
+      initialDeal: function () {
+        for (let i = 0; i < 2; i++) {
+          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+        }
+        this.updatePlayer1Cards();
+      },
+      
+      // Dont forget to add the update player cards method here
+      
+      player1Hit: function() {
+        if (cards.player1Cards.length === 0) {
+            //do nothing    
+        }else{
+          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop()) //take one card from deck
+        }
+        this.updatePlayer1Cards();
+      },
+      
+      updatePlayer1Cards: function() {
+        let html = '<ul>';
+        for (let i = 0; i < cards.player1Cards.length; i++) {
+          html += `<li>${cards.player1Cards[i]}</li>`
+        }
+        html += '</ul>';
+        document.getElementById('player1Cards').innerHTML = html;
+        this.totalPlayer1Cards();
+      },
+     
+      totalPlayer1Cards: function() {
+        for (let i = 0; i < cards.player1Cards.length; i++) {
+          cards.player1CardsTotal += cards.player1Cards[i];
+        }
+        document.getElementById('player1CardsTotal').innerText = cards.player1CardsTotal;
+        if (cards.player1CardsTotal > 21) {
+            cards.busted = 1
+            setTimeout(function() {alert('BUSTED! Give us your money!!')} , 100)
+            cards.player1Cards = [];
+        } else if (cards.player1CardsTotal = 21) {
+            // chill for a bit
+        }
+        cards.player1CardsTotal = 0;
+      },
+
+      newGame: function() {
+        if (cards.player1Cards.length === 0) {
+        this.initialDeal(); 
+        }
+    },
+    
+      player1Stand: function() {
+        this.dealerPlayLogic();
+        },
+  
+      //dealer gets their cards
+  
+      dealerPlayLogic: function() {
+      setTimeout(100)
+        
+        if (player1CardsTotal == 21) {
+              while (dealerCardsTotal <= 21) {
+                cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+              }
+          }
+      },
+
+      initialDealerCards: function() {
+        cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+        // player 1 cards updated 
+        this.updateDealerCards();
+    },
+  
+    // setTimeout(function() {alert('BUSTED! Give us your money!!')} , 100)
+  
+      totalDealerCards: function() {
+        for (let i = 0; i < cards.dealerCards.length; i++) {
+        cards.dealerCardsTotal += cards.dealerCards[i];
+        }
+        document.getElementById('dealerCardsTotal').innerText = cards.dealerCardsTotal;
+    },
+
+      updateDealerCards: function() {
+        let html = '<ul>';
+        for (let i = 0; i < cards.dealerCards.length; i++) {
+          html += `<li>${cards.dealerCards[i]}</li>`
+        }
+        html += '</ul>';
+        document.getElementById('dealerCards').innerHTML = html;
+        this.totalDealerCards();
+    }
+};
+    cardMethod.totalDealerCards();
+    cardMethod.initialDeal();
+
+      
--- blackjackfinal.js
+++ blackjackfinal.js
@@ -11,32 +11,25 @@ let cards = {
   
   let cardMethod = {
       initialDeal: function () {
-        for (let i = 0; i < 2; i++) {
-          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+        for (let i = 0; i < 2; i++) { //iterating twice to give two cards
+          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop()) //removing random whole number out of deck array and pushing it to player1cards array.
         }
-        this.updatePlayer1Cards();
-        
+        this.updatePlayer1Cards(); //calling update card
       },
 
-      newGame: function() {
+      newGame: function() { 
           //clear the DOM
           //initial deal
-          if (cards.player1Cards.length === 0) {
-          this.initialDeal();
+          if (cards.player1Cards.length === 0) { //if there is no cards here
+          this.initialDeal(); //call on inital deal to give two!
           }
       },
-      
-      // Dont forget to add the update player cards method here
-      
-  
+            
       totalPlayer1Cards: function () {
-        for (let i = 0; i < cards.player1Cards.length; i++) {
-          cards.player1CardsTotal += cards.player1Cards[i];
-  
+        for (let i = 0; i < cards.player1Cards.length; i++) { //iterates through length of player1cards
+          cards.player1CardsTotal += cards.player1Cards[i]; //gets sum of each card in player hand
         }
-  
-        document.getElementById('player1CardsTotal').innerText = cards.player1CardsTotal;
-
+        document.getElementById('player1CardsTotal').innerText = cards.player1CardsTotal; //saves it in DOM to display
         if (cards.player1CardsTotal > 21) {
             cards.busted = 1;
             setTimeout(function() { alert('BUSTED! Give us your money!'); }, 100);
@@ -45,68 +38,58 @@ let cards = {
             //chillllllll
         }
         cards.player1CardsTotal = 0;
-  
       },
 
       player1Hit: function() {
-
         if (cards.player1Cards.length === 0) {
             //dont do anything ever. player must hit new game to continue
         } else {
             cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
             this.updatePlayer1Cards();
         }
- 
-  
       },
       
       updatePlayer1Cards: function() { 
         let html = `<ul>`;
         for (let i = 0; i < cards.player1Cards.length; i++) {
           html += `<li>${cards.player1Cards[i]}</li>`
-  
         }
         html += `</ul>`;
         document.getElementById('player1Cards').innerHTML = html;
         this.totalPlayer1Cards();
-  
       },
 
-      player21Stand: function() {
+      player1Stand: function() {
           this.dealerPlayLogic();
       },
 
-      dealerPlayLogic: function() {
+      // //dealerPlayLogic: function() { //uncomment when reworking
           
 
-        setTimeout(function() {
-            if (player1CardsTotal == 21) {
-                while (dealerCardsTotal <= 21) {
-                }
-            }
-        }
+      //   setTimeout(function() {
+      //       if (player1CardsTotal == 21) {
+      //           while (dealerCardsTotal <= 21) {
+      //           }
+      //       }
+      //   }
 
-      },
+      // //}, //end of uncomment
 
       //dealer gets their cards
 
-  
       initialDealerCards: function() {
         for (let i = 0; i < 2; i++) {
           cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
         }
         // player 1 cards updated 
         this.updatePlayer1Cards();
-  
     },
-  
   
       totalDealerCards: function() {
         for (let i = 0; i < cards.dealerCards.length; i++) {
           cards.dealerCardsTotal += cards.dealerCards[i];
         }
         document.getElementById('dealerCardsTotal').innerText = cards.dealerCardsTotal;
-  
       },
   
       //Hit and stand buttons clicked from player 1
