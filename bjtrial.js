1ea2b35ce1b587aaef85a7c04f040c821b0d5b8c
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-18 23:51:04 -0400

    Completed trials and added card files

--- bjtrial.js
+++ bjtrial.js
@@ -1,105 +1,78 @@
-
 let cards = {
-    deck: [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10],
-    player1Cards: [],
-    player1CardsTotal: 0,
-    dealerCards: [],
-    dealerCardsTotal: 0,
-    busted: 0
-  }
-  
-  let cardMethods = {
-      initialDeal: function () {
-        for (let i = 0; i < 2; i++) {
-          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+deck: [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10],
+player1Cards: [],
+player1CardsTotal: 0,
+dealerCards: [],
+dealerCardsTotal: 0,
+busted: 0
+}
+
+let cardMethods = {
+    initialDeal: function () {
+        for (let i = 0; i < 2; i++) { //iterating twice to give two cards
+          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop()) //removing random whole number out of deck array and pushing it to player1cards array.
         }
-        this.updatePlayer1Cards();
+        this.updatePlayer1Cards(); //calling update card
       },
-      
-      // Dont forget to add the update player cards method here
-      
-      player1Hit: function() {
+
+    player1Hit: function() {
         if (cards.player1Cards.length === 0) {
-            //do nothing    
-        }else{
-          cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop()) //take one card from deck
+            //do absolutely nothing!
+        } else {
+            cards.player1Cards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
         }
         this.updatePlayer1Cards();
-      },
-      
-      updatePlayer1Cards: function() {
-        let html = '<ul>';
+    },
+
+    player1Stand: function() {
+        this.dealerPlayLogic(); //not created yet!
+    },
+
+    totalPlayer1Cards: function() {
         for (let i = 0; i < cards.player1Cards.length; i++) {
-          html += `<li>${cards.player1Cards[i]}</li>`
+            cards.player1CardsTotal = cards.player1CardsTotal + cards.player1Cards[i];
         }
-        html += '</ul>';
-        document.getElementById('player1Cards').innerHTML = html;
-        this.totalPlayer1Cards();
-      },
-     
-      totalPlayer1Cards: function() {
-        for (let i = 0; i < cards.player1Cards.length; i++) {
-          cards.player1CardsTotal += cards.player1Cards[i];
-        }
-        document.getElementById('player1CardsTotal').innerText = cards.player1CardsTotal;
+        document.getElementById('player1CardsTotal').innerHTML = cards.player1CardsTotal;
         if (cards.player1CardsTotal > 21) {
-            cards.busted = 1
-            setTimeout(function() {alert('BUSTED! Give us your money!!')} , 100)
+            cards.busted = 1;
+            setTimeout(function() { alert('BUSTED! Give us your money!'); }, 100);
             cards.player1Cards = [];
         } else if (cards.player1CardsTotal = 21) {
-            // chill for a bit
+            // we will not touch this yet
         }
         cards.player1CardsTotal = 0;
-      },
-
-      newGame: function() {
-        if (cards.player1Cards.length === 0) {
-        this.initialDeal(); 
-        }
-    },
-    
-      player1Stand: function() {
-        this.dealerPlayLogic();
-        },
-  
-      //dealer gets their cards
-  
-      dealerPlayLogic: function() {
-      setTimeout(100)
-        
-        if (player1CardsTotal == 21) {
-              while (dealerCardsTotal <= 21) {
-                cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
-              }
-          }
-      },
-
-      initialDealerCards: function() {
-        cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
-        // player 1 cards updated 
-        this.updateDealerCards();
-    },
-  
-    // setTimeout(function() {alert('BUSTED! Give us your money!!')} , 100)
-  
-      totalDealerCards: function() {
-        for (let i = 0; i < cards.dealerCards.length; i++) {
-        cards.dealerCardsTotal += cards.dealerCards[i];
-        }
-        document.getElementById('dealerCardsTotal').innerText = cards.dealerCardsTotal;
     },
 
-      updateDealerCards: function() {
+    updatePlayer1Cards: function() {
         let html = '<ul>';
-        for (let i = 0; i < cards.dealerCards.length; i++) {
-          html += `<li>${cards.dealerCards[i]}</li>`
+        for (let i = 0; i < cards.player1Cards.length; i++) {
+            html = html + `<li>${cards.player1Cards[i]}</li>`;
         }
-        html += '</ul>';
-        document.getElementById('dealerCards').innerHTML = html;
-        this.totalDealerCards();
-    }
-};
-    cardMethod.totalDealerCards();
-    cardMethod.initialDeal();
+        html = html + '</ul>';
+        document.getElementById('player1Cards').innerHTML = html;
+        this.totalPlayer1Cards();
+    },
 
-      
+    newGame: function() {
+        if (cards.player1Cards.length === 0) {
+        this.initialDeal();
+        }
+    },
+
+   initialDealerCards: function() {
+        for (let i = 0; i < 2; i++) {
+            cards.dealerCards.push(cards.deck.splice(Math.floor(Math.random() * cards.deck.length), 1).pop())
+            }
+            this.updatePlayer1Cards();
+    },
+
+    totalDealerCards: function() {
+        for (let i = 0; i < cards.dealerCards.length; i++) {
+            cards.dealerCardsTotal = cards.dealerCardsTotal + cards.dealerCards[i];
+        }
+        document.getElementById('dealerCardsTotal').innerHTML = cards.dealerCardsTotal;
+    }
+
+}
+
+cardMethods.initialDeal();
---
+++ cards.js
@@ -0,0 +1,11 @@
+let header = document.createElement('h1');
+header.innerText = 'BLACK JACK GAME!';
+document.body.appendChild(header); //please add a new element to the page called header tag
+
+let playerDiv = document.createElement('div');
+playerDiv.innerText = 'Player 1';
+document.body.appendChild(playerDiv);
+
+let dealerDiv = document.createElement('div');
+dealerDiv.innerText = 'Dealer';
+document.body.appendChild(dealerDiv);
